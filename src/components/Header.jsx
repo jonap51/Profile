23cb7d5a1f89b41934e20e661c49cc1ae2167744
jonap51 <scilicet.jp@gmail.com>
2025-03-19 22:52:43 -0300

    armando estructura primera linea

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <p>
+                    Jonatan
+                </p>
+            </div>
+            <nav>
+                <a href="#">Inicio</a>
+                <a href="#">Servicios</a>
+                <a href="#">Sobre mi</a>
+                <a href="#">contactame</a>
+            </nav>
+
+        </div>
+    )
+}
+
+export { Header }
